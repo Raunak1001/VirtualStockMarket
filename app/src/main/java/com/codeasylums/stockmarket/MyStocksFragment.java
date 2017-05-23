3a95a0cb6e80949e70b96eecffe9bcbc1120a3d6
rajatjain-21 <rjrox.21@gmail.com>
2017-05-23 22:24:51 +0530

    My stocks  commit

--- app/src/main/java/com/codeasylums/stockmarket/MyStocksFragment.java
+++ app/src/main/java/com/codeasylums/stockmarket/MyStocksFragment.java
@@ -1,30 +1,33 @@
 package com.codeasylums.stockmarket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MyStocksFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MyStocksFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MyStocksFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    TextView textView;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager ShareTransactionObjectLayoutManager;
+    private MyStocksAdapter                 ShareTransactionObjectAdapter;
+    List<ShareTransactionObject> ShareTransactionObjectList = new ArrayList<>();
+    FragmentTransaction ft;
 
     private OnFragmentInteractionListener mListener;
 
@@ -32,15 +35,6 @@ public class MyStocksFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyStocksFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MyStocksFragment newInstance(String param1, String param2) {
         MyStocksFragment fragment = new MyStocksFragment();
         Bundle args = new Bundle();
@@ -57,13 +51,26 @@ public class MyStocksFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_stocks, container, false);
+        View rootview=inflater.inflate(R.layout.fragment_my_stocks, container, false);
+        ShareTransactionObjectLayoutManager= new LinearLayoutManager(getActivity());
+        recyclerView=(RecyclerView)rootview.findViewById(R.id.rview);
+        //textView=(TextView) rootview.findViewById(R.id.textView2) ;
+        SQLiteHandler sqliteHandler=new SQLiteHandler(getActivity());
+        recyclerView.setLayoutManager(ShareTransactionObjectLayoutManager);
+       ShareTransactionObjectList=sqliteHandler.getSharedData();
+        ShareTransactionObjectAdapter = new MyStocksAdapter(ShareTransactionObjectList,getActivity());
+        recyclerView.setAdapter(ShareTransactionObjectAdapter);
+        return rootview;
+
     }
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -86,18 +93,17 @@ public class MyStocksFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
+
+
+
+
+
 }
+
+
